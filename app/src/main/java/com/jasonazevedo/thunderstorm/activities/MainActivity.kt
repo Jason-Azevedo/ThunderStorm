4124c5eb7befe71069b35297956fe7f85c94c983
Jason Azevedo <jasonazevedo03@gmail.com>
2022-04-01 11:55:33 +0200

    Added workmanager lib and created Weather Forecast worker class

--- app/src/main/java/com/jasonazevedo/thunderstorm/activities/MainActivity.kt
+++ app/src/main/java/com/jasonazevedo/thunderstorm/activities/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.jasonazevedo.thunderstorm
+package com.jasonazevedo.thunderstorm.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.jasonazevedo.thunderstorm.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
---
+++ app/src/main/java/com/jasonazevedo/thunderstorm/model/WeatherForecast.kt
@@ -0,0 +1,4 @@
+package com.jasonazevedo.thunderstorm.model
+
+// Temporary value to get rid of error
+data class WeatherForecast(val temp: Int)
